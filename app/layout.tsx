d337e3a0eb4e0bc328434896cf1a8dd976a4f732
oleeeedev <oleschumacherr1801@gmail.com>
2025-02-04 21:49:08 +0100

    Add Footer and Hero components to layout and homepage
    Introduced a Footer component with detailed styles and content links, enhancing the layout structure. Integrated a Hero component on the homepage for improved visual presentation. This update refines the UI/UX for better user engagement.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 import { Inter } from "next/font/google";
 import "node_modules/react-modal-video/css/modal-video.css";
 import "../styles/index.css";
@@ -18,8 +19,8 @@ export default function RootLayout({
 
     <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
     <Providers>
-      <Header />
       {children}
+      <Footer />
     </Providers>
     </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 import { Metadata } from "next";
 
+import Hero from "@/components/Hero";
+
+
 export const metadata: Metadata = {
   title: "",
   description: "",
-  // other metadata
 };
 
 export default function Home() {
   return (
     <>
+      <Hero />
     </>
   );
 }
