050eea93bfa4d261e5d804fa2adea2647a999e28
oleeeedev <oleschumacherr1801@gmail.com>
2025-02-04 19:35:52 +0100

    Add initial menu types, data, and header component structure
    Introduced a `Menu` type definition to structure menu data. Added `menuData` with hierarchical menu items and a basic `Header` component setup. These additions lay the foundation for navigation-related features.

---
+++ components/Header/index.tsx
@@ -0,0 +1,15 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname} from "next/navigation";
+
+const Header = () => {
+  return (
+    <>
+    <header
+      className={''}
+      >
+
+    </header></>
+    )
+}
