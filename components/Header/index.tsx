d682b4a4f4a62566410bc008225760b25a0b54f1
oleeeedev <oleschumacherr1801@gmail.com>
2025-02-04 19:59:13 +0100

    Add ThemeToggler and enhance header functionality
    Introduced a `ThemeToggler` component to enable theme switching. Enhanced the header with responsiveness, sticky behavior, and submenu toggle logic. Added two logo SVG files for improved UI branding.

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,8 +2,38 @@
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname} from "next/navigation";
+import { useEffect, useState } from "react";
+import menuData from "./menuData";
+import ThemeToggler from "./ThemeToggler";
 
 const Header = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const navbarToggleHandler = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+
+  const [sticky, setSticky] = useState(false);
+  const handleSticksNavbar = () => {
+    if (window.scrollY >= 80) {
+      setSticky(true);
+    } else {
+      setSticky(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleSticksNavbar);
+  }, []);
+
+  const [openIndex, setOpenIndex] = useState(-1);
+  const handleSubmenu = (index) => {
+    if (openIndex === index) {
+      setOpenIndex(-1);
+    } else {
+      setOpenIndex(index);
+    }
+  };
+
+
   return (
     <>
     <header
@@ -13,3 +43,5 @@ const Header = () => {
     </header></>
     )
 }
+
+export default Header;
