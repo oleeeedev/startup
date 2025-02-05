046e1a5b540b16db0fbb92a669d9fd14c680fdc5
oleeeedev <oleschumacherr1801@gmail.com>
2025-02-05 11:49:47 +0100

    Add Brands component with responsive brand logos.
    This includes a new `Brands` component for displaying a grid of brand logos with hover effects. It uses data from `brandsData` and a newly defined `Brand` type. Associated SVG assets were added for both light and dark themes.

---
+++ components/Brands/brandsData.tsx
@@ -0,0 +1,34 @@
+import { Brand } from "@/types/brand";
+
+const brandsData: Brand[] = [
+  {
+    id: 1,
+    name: "Lineicons",
+    href: "https://lineicons.com/",
+    image: "/images/brands/lineicons.svg",
+    imageLight: "/images/brands/lineicons-light.svg",
+  },
+  {
+    id: 2,
+    name: "Lineicons",
+    href: "https://lineicons.com/",
+    image: "/images/brands/lineicons.svg",
+    imageLight: "/images/brands/lineicons-light.svg",
+  },
+  {
+    id: 3,
+    name: "Lineicons",
+    href: "https://lineicons.com/",
+    image: "/images/brands/lineicons.svg",
+    imageLight: "/images/brands/lineicons-light.svg",
+  },
+  {
+    id: 4,
+    name: "Lineicons",
+    href: "https://lineicons.com/",
+    image: "/images/brands/lineicons.svg",
+    imageLight: "/images/brands/lineicons-light.svg",
+  },
+];
+
+export default brandsData;
