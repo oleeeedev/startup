b6f926fa3f67c7449fd94695d6555dbcf67f8a53
oleeeedev <oleschumacherr1801@gmail.com>
2025-02-05 13:35:45 +0100

    Add Testimonials component with associated types and assets
    Implemented the `Testimonials` section, including reusable `SingleTestimonial` and sample data. Added the `Testimonial` type for better structure and consistency. Included related images and assets to visually represent testimonials.

---
+++ components/Testimonials/index.tsx
@@ -0,0 +1,13 @@
+import { Testimonial} from "@/types/testimonial";
+
+const testimonialsData: Testimonial[] = [
+  {
+    id: 1,
+    name: "Max Mustermann",
+    designation: "Founder @Test",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, tortor quis tincidunt egestas, metus odio tincidunt est, quis lacinia ante odio a lectus.",
+    image: "/images/testimonials/testimonial-1.jpg",
+    star: 5,
+  },
+];
